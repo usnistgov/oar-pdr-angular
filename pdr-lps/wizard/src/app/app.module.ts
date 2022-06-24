e66a6c8aa3bcf56fcb3f1db245313969e43d608a
chuanlin2018 <c.lin@prometheuscomputing.com>
2022-06-24 14:32:55 -0400

    Added pdr step wizard

---
+++ pdr-lps/wizard/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StepWizModule } from './startwiz/stepwizard.module';
+import { WizardModule } from 'oarng';
+import { OARngModule } from 'oarng';
+import { InputTextModule } from "primeng/inputtext";
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        StepWizModule,
+        AppRoutingModule,
+        WizardModule,
+        InputTextModule,
+        OARngModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
