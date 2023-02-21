3f8ad4936269c275a8539bfa2eee118c785631f6
elmiomar <omarilias.elmimouni@nist.gov>
2023-02-21 10:01:13 -0500

    add approve app code

---
+++ pdr-rpa/pdr-rpa-approve/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ErrorsModule, NotFoundComponent } from 'oarlps';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+    {path: 'approve', component: AppComponent},
+    { path: '**', component: NotFoundComponent}
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes, {
+            initialNavigation: 'enabled',
+            relativeLinkResolution: 'corrected'
+        }),
+        ErrorsModule
+    ],
+    exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
+
--- pdr-rpa/pdr-rpa-approve/src/app/app.component.ts
+++ pdr-rpa/pdr-rpa-approve/src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/api';
+
+import { ActivatedRoute, Router } from '@angular/router';
+import { Record } from './model/record';
+import { RPAService } from './service/rpa.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [MessageService, RPAService]
 })
 export class AppComponent {
-  title = 'pdr-rpa-request';
+  recordId: string;
+  status: string;
+  statusDate: string;
+  record: Record;
+  loaded: boolean = false;
+  displayProgressSpinner: boolean = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private rpaService: RPAService,
+    private messageService: MessageService
+  ) { }
+
+  ngOnInit(): void {
+    // get the recordId from the url
+    // fetch the record using the recordId
+    // check the approval status
+    // use appropriate template based on status
+    this.status = "pending";
+    this.route.queryParams.subscribe(params => {
+      this.displayProgressSpinner = false;
+      this.recordId = params['id'];
+      if (this.recordId) {
+        console.log(this.recordId);
+        this.rpaService.getRecord(this.recordId).subscribe(data => {
+          this.record = data.record;
+          this.status = this.record.userInfo.approvalStatus;
+          this.statusDate = this.status.replace("Approved_", "");
+          this.loaded = true;
+          console.log(this.record)
+        });
+      }
+
+    });
+  }
+
+  /**
+   * Approve rpa request and display success message
+   */
+  onApprove(): void {
+    this.displayProgressSpinner = true;
+    this.rpaService.approveRequest(this.recordId).subscribe(data => {
+      this.displayProgressSpinner = false;
+      console.log("Approved!\n", data)
+      this.messageService.add({ severity: 'success', summary: 'Success', detail: 'This request was approved successfully!' });
+      location.reload();
+    });
+  }
+
+  /**
+   * Decline rpa request and display success message
+   */
+  onDecline(): void {
+    this.displayProgressSpinner = true;
+    this.rpaService.declineRequest(this.recordId).subscribe(data => {
+      this.displayProgressSpinner = false;
+      console.log("Declined!\n", data)
+      this.statusDate = data.approvalStatus.replace("Approved_", "");
+      this.messageService.add({ severity: 'success', summary: 'Success', detail: 'This request was declined successfully!' });
+      location.reload();
+    });
+  }
 }
