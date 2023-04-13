15d872a554e34a9059180874ae3c64adbf32c648
chuanlin2018 <c.lin@prometheuscomputing.com>
2023-04-13 15:04:16 -0400

    Some clean up for Jest.

--- oar-lps/libs/oarlps/jest.config copy.js
+++
@@ -1,8 +0,0 @@
-const baseConfig = require('../../../jestx.base.config2');
-module.exports = {
-    ...baseConfig,
-    moduleNameMapper: {
-        "^lodash-es$": "lodash"
-    },
-    globalSetup: 'jest-preset-angular/global-setup'
-};
