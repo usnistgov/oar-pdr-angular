2f5a55a5ae12dbc27ac6b3fa6dc3da5cb2ac5b8e
chuanlin2018 <c.lin@prometheuscomputing.com>
2023-08-02 15:02:40 -0400

    Fixed environment setting.

--- midas-author/lps/src/environments/environment.ts
+++ midas-author/lps/src/environments/environment.ts
@@ -27,11 +27,11 @@ export const config: LPSConfig = {
         mdService:   "https://oardev.nist.gov/rmm/",
         taxonomyService: "https://oardev.nist.gov/rmm/taxonomy"
     },
-    mdAPI: "http://localhost:9091/midas/dap/mds3/",
+    mdAPI: "http://localhost/midas/dap/mds3/",
     // mdAPI: "https://mdsdev.nist.gov/midas/dap/mds3/",
     metricsAPI: "https://data.nist.gov/rmm/usagemetrics/",
     // customizationAPI: "https://testdata.nist.gov/customization/",
-    customizationAPI: "http://localhost:9091/midas/",
+    customizationAPI: "http://localhost/midas/",
     // customizationAPI: "https://mdsdev.nist.gov/midas/",
     fileManagerAPI: "https://nextcloud-dev.nist.gov",
     mode: "dev",
