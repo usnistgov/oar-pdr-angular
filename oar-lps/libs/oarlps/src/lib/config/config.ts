2a5966fceb0357ce663896d4ef2a072b1e473b70
RayPlante <raymond.plante@nist.gov>
2024-12-05 10:54:07 -0500

    config/config.ts: turn into an import compatibility layer for AppConfig

--- oar-lps/libs/oarlps/src/lib/config/config.ts
+++ oar-lps/libs/oarlps/src/lib/config/config.ts
@@ -1,14 +1,10 @@
 /**
  * Classes used to support the configuration infrastructure.  
  * 
- * Configuration parameters used by the application are defined in the form of
- * interfaces.  The AppConfig is an implementation of the app-level configuration
- * interface, LPSConfig, that can be injected into Components.  
+ * This file provides import compatibility with the previous implementation (see config.module).
  */
 import { Injectable } from '@angular/core';
 import { LPSConfig } from './config.model';
-import { ConfigService } from './config.service';
-
-class AppConfig extends ConfigService { }
+import { AppConfig } from './config.service';
 
 export { LPSConfig, AppConfig }
