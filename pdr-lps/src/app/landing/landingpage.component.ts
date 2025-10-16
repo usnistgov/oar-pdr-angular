c6a859b01a6321e056ea6d9d84c92a1b8e4ec68e
chuanlin2018 <c.lin@prometheuscomputing.com>
2025-10-16 14:21:35 -0400

    Disable router navigate from server side.

--- pdr-lps/src/app/landing/landingpage.component.ts
+++ pdr-lps/src/app/landing/landingpage.component.ts
@@ -358,25 +358,30 @@ export class LandingPageComponent implements OnInit, AfterViewInit {
                 }
             }
 
-            if(this.inBrowser)
-                this._showContent = true;
+                if (this.inBrowser) {
+                    this._showContent = true;
                 
-            if (metadataError == "not-found") {
-                this.router.navigateByUrl("not-found/" + this.reqId, { skipLocationChange: true });
-            }
+                    if (metadataError == "not-found") {
+                        this.router.navigateByUrl("not-found/" + this.reqId, { skipLocationChange: true });
+                    }
+                }
         },
         (err) => {
             this.globalService.setShowLPContent(true);
             if (err instanceof IDNotFound) {
                 console.error("ID not found - navigating to not-found page: ", err);
-                metadataError = "not-found";
-                this.router.navigateByUrl("not-found/" + this.reqId, { skipLocationChange: true });
+                if (this.inBrowser) {
+                    metadataError = "not-found";
+                    this.router.navigateByUrl("not-found/" + this.reqId, { skipLocationChange: true });
+                }
             }
             else {
                 console.error("Failed to retrieve metadata: ", err);
-                metadataError = "int-error";
-                // this.router.navigateByUrl("int-error/" + this.reqId, { skipLocationChange: true });
-                this.router.navigateByUrl("int-error/" + this.reqId, { skipLocationChange: true });
+                if (this.inBrowser) {
+                    metadataError = "int-error";
+                    // this.router.navigateByUrl("int-error/" + this.reqId, { skipLocationChange: true });
+                    this.router.navigateByUrl("int-error/" + this.reqId, { skipLocationChange: true });
+                }
             }
         });
     }
