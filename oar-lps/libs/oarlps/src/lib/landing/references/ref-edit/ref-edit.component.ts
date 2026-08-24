8e09d95e5f96f75e271e124bc07c8bc8ff5d23ae
chuanlin2018 <c.lin@prometheuscomputing.com>
2026-08-24 10:16:46 -0400

    All Primeng related items removed.

--- oar-lps/libs/oarlps/src/lib/landing/references/ref-edit/ref-edit.component.ts
+++ oar-lps/libs/oarlps/src/lib/landing/references/ref-edit/ref-edit.component.ts
@@ -6,6 +6,7 @@ import { MetadataUpdateService } from '../../editcontrol/metadataupdate.service'
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RefAuthorComponent } from '../ref-author/ref-author.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TextEditComponent } from '../../../text-edit/text-edit.component';
 import { AppConfig } from '../../../config/config';
 import { AuthenticationService } from 'oarng';
@@ -23,52 +24,56 @@ import {
     faLock,
     faUnlock
 } from '@fortawesome/free-solid-svg-icons';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-    selector: 'lib-ref-edit',
+    selector: "lib-ref-edit",
     standalone: true,
     imports: [
         CommonModule,
         FormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatTooltipModule,
+        MatRadioModule,
+        MatIconModule,
         TextEditComponent,
+        NgbModule,
         RefAuthorComponent,
         SingleMsgBarComponent,
         FontAwesomeModule,
-        MatButtonModule,
-        MatTooltipModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatRadioModule,
-        MatCheckboxModule
     ],
-    templateUrl: './ref-edit.component.html',
-    styleUrls: ['../../landing.component.scss', './ref-edit.component.css'],
+    templateUrl: "./ref-edit.component.html",
+    styleUrls: ["../../landing.component.scss", "./ref-edit.component.css"],
     animations: [
-        trigger('editExpand', [
-        state('collapsed', style({height: '0px', minHeight: '0'})),
-        state('expanded', style({height: '*'})),
-        transition('expanded <=> collapsed', animate('625ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-        ])
-    ]
+        trigger("editExpand", [
+            state("collapsed", style({ height: "0px", minHeight: "0" })),
+            state("expanded", style({ height: "*" })),
+            transition(
+                "expanded <=> collapsed",
+                animate("625ms cubic-bezier(0.4, 0.0, 0.2, 1)"),
+            ),
+        ]),
+    ],
 })
 export class RefEditComponent implements OnInit {
     originalRef: Reference = {} as Reference;
     defaultText: string = "Enter citation here";
     reftype: string = "1";
-    fieldName: string = 'references';
-    
+    fieldName: string = "references";
+
     // Input method. 1 = DOI, 2=Ref data, 3=Citation text
-    inputMethod: string = "2"; 
+    inputMethod: string = "2";
 
     // contentCollapsed: boolean = true;
     citationLocked: boolean = false;
-    editBlockStatus: string = 'collapsed';
+    editBlockStatus: string = "collapsed";
 
     showAllFields: boolean = false;
     ref: Reference = {} as Reference;
@@ -98,7 +103,7 @@ export class RefEditComponent implements OnInit {
     doiEndpoint: string = "";
     doiLoaded: boolean = false;
     authToken: string = "";
-    errMessage: string
+    errMessage: string;
 
     @Input() currentRef: Reference = {} as Reference;
     @Input() editMode: string = "edit";
@@ -107,13 +112,13 @@ export class RefEditComponent implements OnInit {
     @Output() cmdOutput: EventEmitter<any> = new EventEmitter();
 
     constructor(
-        private httpClient: HttpClient, 
-        private chref: ChangeDetectorRef,  
+        private httpClient: HttpClient,
+        private chref: ChangeDetectorRef,
         private cfg: AppConfig,
         public authsvc: AuthenticationService,
         public iconLibrary: FaIconLibrary,
-        public mdupdsvc: MetadataUpdateService) { 
-        
+        public mdupdsvc: MetadataUpdateService,
+    ) {
         iconLibrary.addIcons(
             faPencil,
             faXmark,
@@ -123,40 +128,43 @@ export class RefEditComponent implements OnInit {
             faCaretDown,
             faCaretRight,
             faLock,
-            faUnlock
-        );   
+            faUnlock,
+        );
 
-        this.doiEndpoint = cfg.get<string>("dapEditing.doiEndpoint", "/midas/doi/2nerdm/ref/");
-    
-        this.authsvc.getCredentials().subscribe(
-            (creds) => {
-                this.authToken = creds.token;
-            }
-        )
+        this.doiEndpoint = cfg.get<string>(
+            "dapEditing.doiEndpoint",
+            "/midas/doi/2nerdm/ref/",
+        );
+
+        this.authsvc.getCredentials().subscribe((creds) => {
+            this.authToken = creds.token;
+        });
     }
 
     ngOnInit(): void {
-        if(this.isEditing) this.showAllFields = true;
-        if(this.currentRef) {
+        if (this.isEditing) this.showAllFields = true;
+        if (this.currentRef) {
             this.originalRef = JSON.parse(JSON.stringify(this.currentRef));
             this.ref = JSON.parse(JSON.stringify(this.currentRef));
-            this.reftype = this.currentRef.refType == "IsSupplementTo" ? "1" : "2" ;
+            this.reftype =
+                this.currentRef.refType == "IsSupplementTo" ? "1" : "2";
         }
     }
 
     ngOnChanges(changes: SimpleChanges): void {
         //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
         //Add '${implements OnChanges}' to the class.
-        if(changes.editMode && changes.editMode.currentValue == "normal") {
+        if (changes.editMode && changes.editMode.currentValue == "normal") {
             this.reset();
         }
 
-        if(changes.currentRef) {
-            if(this.currentRef) {
+        if (changes.currentRef) {
+            if (this.currentRef) {
                 this.originalRef = JSON.parse(JSON.stringify(this.currentRef));
                 this.ref = JSON.parse(JSON.stringify(this.currentRef));
-                this.reftype = this.originalRef.refType == "IsSupplementTo" ? "1" : "2" ;
-            }else{
+                this.reftype =
+                    this.originalRef.refType == "IsSupplementTo" ? "1" : "2";
+            } else {
                 this.originalRef = undefined;
                 this.ref = {} as Reference;
             }
@@ -165,46 +173,56 @@ export class RefEditComponent implements OnInit {
         this.chref.detectChanges();
     }
 
-    get isEditing() { return this.editMode=="edit" };
-    get isAdding() { return this.editMode=="add" };
-    get useDOI() { return this.inputMethod == "1" };
-    get useRefData() { return this.inputMethod == "2" };
-    get useCitationData() { return this.inputMethod == "3" };
+    get isEditing() {
+        return this.editMode == "edit";
+    }
+    get isAdding() {
+        return this.editMode == "add";
+    }
+    get useDOI() {
+        return this.inputMethod == "1";
+    }
+    get useRefData() {
+        return this.inputMethod == "2";
+    }
+    get useCitationData() {
+        return this.inputMethod == "3";
+    }
 
     /**
      * When edit block expanded, set overflow to visible so tooltip can be seen.
-     * @returns 
+     * @returns
      */
     getAuthorStyle() {
-        if(this.editBlockStatus == 'collapsed')
-            return {'overflow': 'hidden'};
-        else{
-            return {'overflow': 'visible'};
+        if (this.editBlockStatus == "collapsed") return { overflow: "hidden" };
+        else {
+            return { overflow: "visible" };
         }
     }
 
     reset() {
-        this.inputMethod = "2"; 
+        this.inputMethod = "2";
 
         this.citationLocked = false;
-        this.editBlockStatus = 'collapsed';
+        this.editBlockStatus = "collapsed";
         this.doiLoaded = false;
-    
+
         this.showAllFields = false; // Show all fields but doi
-     
+
         this.chref.detectChanges();
     }
 
-    onChange(updateCitation:boolean = false) {
+    onChange(updateCitation: boolean = false) {
         this.ref.dataChanged = true;
 
-        if(updateCitation) this.updateCitation();
+        if (updateCitation) this.updateCitation();
 
-        this.dataChanged.emit({"ref": this.ref, "dataChanged": true});
+        this.dataChanged.emit({ ref: this.ref, dataChanged: true });
     }
 
     onReftypeChanged(event) {
-        this.ref.refType = event.target.value=="1" ? "IsSupplementTo" : "References";
+        this.ref.refType =
+            event.target.value == "1" ? "IsSupplementTo" : "References";
         this.onChange(false);
     }
 
@@ -212,55 +230,71 @@ export class RefEditComponent implements OnInit {
      * On DOI field changed, fetch ref data from backend then populate ref data.
      * Set this.refDataPopulated to true.
      */
-    onDoiChange(event) {
-        this.ref.doi = event.value;
+    onDoiChange() {
+        // this.ref.doi = event.value;
 
         // Remove the beginning slash if any
         if (this.ref.doi.startsWith("/")) this.ref.doi = this.ref.doi.slice(1);
 
         const url = this.doiEndpoint + this.ref.doi;
-        const hdrs = {"Accept":"application/json", "Authorization":"Bearer " + this.authToken};
+        const hdrs = {
+            Accept: "application/json",
+            Authorization: "Bearer " + this.authToken,
+        };
 
-        this.httpClient.get(url, {headers: hdrs}).subscribe(
-            data => {
-                if (data["pdr:comment"] && data["pdr:comment"] == "DOI does not exist yet") {
+        this.httpClient.get(url, { headers: hdrs }).subscribe(
+            (data) => {
+                if (
+                    data["pdr:comment"] &&
+                    data["pdr:comment"] == "DOI does not exist yet"
+                ) {
                     this.errMessage = "DOI not found: " + this.ref.doi;
-                    
-                    // Show "Reference data interface" 
+
+                    // Show "Reference data interface"
                     this.citationLocked = false;
                     this.showAllFields = true;
                 } else {
                     let doi = this.ref.doi;
                     this.ref = JSON.parse(JSON.stringify(data)) as Reference;
                     if (!this.ref.doi) this.ref.doi = doi;
-                    
+
                     this.citationLocked = true;
                     this.onChange(true);
                     this.showAllFields = false;
                     this.doiLoaded = true;
                 }
             },
-            err => { 
+            (err) => {
                 let msg = err.message || err.statusText;
                 if (err.status) {
-                    if (err.status == 404) 
+                    if (err.status == 404)
                         this.errMessage = "DOI not found: " + this.ref.doi;
-                    if (err.status == 401) 
-                        this.errMessage = "Unauthorized access. Please login again.";
-                    if (err.status > 500) 
-                        this.errMessage = "Server error occurred when retrieving DOI data: " + this.ref.doi;
-                    else 
-                        this.errMessage = "Unexpected resolver response: " + err.message +" (" +
-                                                  err.status + ")";
-                }
-                else if (err.status == 0) 
-                    this.errMessage = "Unexpected communication error: " + err.message + ".";
+                    if (err.status == 401)
+                        this.errMessage =
+                            "Unauthorized access. Please login again.";
+                    if (err.status > 500)
+                        this.errMessage =
+                            "Server error occurred when retrieving DOI data: " +
+                            this.ref.doi;
+                    else
+                        this.errMessage =
+                            "Unexpected resolver response: " +
+                            err.message +
+                            " (" +
+                            err.status +
+                            ")";
+                } else if (err.status == 0)
+                    this.errMessage =
+                        "Unexpected communication error: " + err.message + ".";
                 else
-                    this.errMessage = "Unexected error during retrieval from URL (" + 
-                        url + "): " + err.toString();
-                
+                    this.errMessage =
+                        "Unexected error during retrieval from URL (" +
+                        url +
+                        "): " +
+                        err.toString();
+
                 console.warn(err.message + " DOI:" + this.ref.doi);
-            }
+            },
         );
     }
 
@@ -276,8 +310,8 @@ export class RefEditComponent implements OnInit {
     }
 
     /**
-     * 
-     * @param index 
+     *
+     * @param index
      */
     removeAuthor(index: number) {
         this.ref.authors.splice(index, 1);
@@ -287,11 +321,11 @@ export class RefEditComponent implements OnInit {
     setInputMethod(event) {
         var target = event.target;
         this.inputMethod = event.target.value;
-        
-        switch ( this.inputMethod ) {
+
+        switch (this.inputMethod) {
             case "1": //useDOI
                 // By default, citation is not linked to other fields. User can edit content.
-                this.citationLocked = true; 
+                this.citationLocked = true;
                 this.showAllFields = false;
                 break;
             case "2": //useRefData
@@ -308,39 +342,39 @@ export class RefEditComponent implements OnInit {
     }
 
     /**
-     * 
+     *
      * @returns Citation lock icon class based on editing status
      */
     citationLockClass() {
-        if(this.citationLocked) {
+        if (this.citationLocked) {
             return this.lockIcon;
-        }else{
+        } else {
             return this.unlockIcon;
         }
     }
 
     expandIconClass() {
-        if(this.editBlockStatus == "collapsed") {
+        if (this.editBlockStatus == "collapsed") {
             return this.caretRightIcon;
-        }else{
+        } else {
             return this.caretDownIcon;
         }
     }
 
     /**
-     * 
+     *
      * @returns tooltip of lock icon based on editing status
      */
     citationLockTooltip() {
-        if(this.citationLocked) {
+        if (this.citationLocked) {
             return "Click to unlock citation";
-        }else{
+        } else {
             return "Click to lock citation";
         }
     }
 
     /**
-     * When reference data changed, set the flag so 
+     * When reference data changed, set the flag so
      */
     onDataChange(dataChanged: boolean) {
         this.onChange(true);
@@ -351,71 +385,70 @@ export class RefEditComponent implements OnInit {
      * If it's locked, citation text will be entered by user manually.
      */
     updateCitation() {
-        if(this.citationLocked) return;
-
+        if (this.citationLocked) return;
 
         this.ref.citation = "";
         // Add authors
-        if(this.ref.authors && this.ref.authors.length > 0){
-            for(let i = 0; i <= this.ref.authors.length-1; i++) {
+        if (this.ref.authors && this.ref.authors.length > 0) {
+            for (let i = 0; i <= this.ref.authors.length - 1; i++) {
                 this.ref.citation += this.ref.authors[i];
-                if(i < this.ref.authors.length-2) this.ref.citation += ", ";
-                if(i == this.ref.authors.length-2) this.ref.citation += ", & ";
-                if(i == this.ref.authors.length-1) this.ref.citation += " ";
+                if (i < this.ref.authors.length - 2) this.ref.citation += ", ";
+                if (i == this.ref.authors.length - 2)
+                    this.ref.citation += ", & ";
+                if (i == this.ref.authors.length - 1) this.ref.citation += " ";
             }
         }
 
         // Add publish year
-        if(this.ref.publishYear)
+        if (this.ref.publishYear)
             this.ref.citation += "(" + this.ref.publishYear + "). ";
 
         // Add title
-        if(this.ref.title)
-            this.ref.citation += this.ref.title + ". ";
+        if (this.ref.title) this.ref.citation += this.ref.title + ". ";
 
         // Add Journal
-        if(this.ref.label)
-            this.ref.citation += this.ref.label + ". ";
+        if (this.ref.label) this.ref.citation += this.ref.label + ". ";
 
         let addPeriod: boolean = false;
         // Add Vol
-        if(this.ref.vol){
-            this.ref.citation += this.ref.vol;  
+        if (this.ref.vol) {
+            this.ref.citation += this.ref.vol;
             addPeriod = true;
         }
-            
+
         // Add Vol number
-        if(this.ref.volNumber){
-            this.ref.citation += "(" + this.ref.volNumber + ")";  
+        if (this.ref.volNumber) {
+            this.ref.citation += "(" + this.ref.volNumber + ")";
             addPeriod = true;
         }
 
         // Add pages
-        if(this.ref.pages){
-            this.ref.citation += " " + this.ref.pages;  
+        if (this.ref.pages) {
+            this.ref.citation += " " + this.ref.pages;
             addPeriod = true;
         }
 
-        if(addPeriod) this.ref.citation += ". "; 
+        if (addPeriod) this.ref.citation += ". ";
 
         // Add URL
-        if(this.ref.doi) {
+        if (this.ref.doi) {
             this.ref.citation += "doi: " + this.ref.doi;
-        }else if(this.ref.location) {
+        } else if (this.ref.location) {
             this.ref.citation += "doi: " + this.ref.location;
         }
 
         // In preparation
-        if(this.ref.inPreparation == "yes") {
-            this.ref.citation += ". In preparation."
+        if (this.ref.inPreparation == "yes") {
+            this.ref.citation += ". In preparation.";
         }
     }
 
     /**
-     * Show/hide author edit block when user clicks on the icon. 
+     * Show/hide author edit block when user clicks on the icon.
      */
     authorExpandClick() {
-        this.editBlockStatus = this.editBlockStatus=="collapsed"? "expanded" : "collapsed";
+        this.editBlockStatus =
+            this.editBlockStatus == "collapsed" ? "expanded" : "collapsed";
         this.chref.detectChanges();
     }
 
@@ -424,6 +457,6 @@ export class RefEditComponent implements OnInit {
      * @param cmd command
      */
     commandOut(cmd: string) {
-        this.cmdOutput.emit({"command": cmd});
-    }  
+        this.cmdOutput.emit({ command: cmd });
+    }
 }
